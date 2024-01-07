fd7113a395b1529348a72ebd2eea10368a2a5111
Rohan1279 <muniruddinrohan@gmail.com>
2024-01-07 18:09:50 +0600

    Refactor Careers component layout and add Apply Now button

--- src/Pages/Home/Careers/Careers.jsx
+++ src/Pages/Home/Careers/Careers.jsx
@@ -35,19 +35,43 @@ const Careers = () => {
               <span className="">⭐⭐⭐⭐⭐</span>
             </div>
 
-            <div className="flex justify-start items-start space-x-4 mt-4">
-              <img src={company2} alt="" className="w-32" />
-              <div className="flex flex-col bg-red h-full">
-                <div className="tex">
-                  <h3 className="font-bold text-3xl mb-2">UI/UX Designer</h3>
-                  <div className="flex justify-between">
-                    <img src={location} alt="" className="w-3 h-3" />
-                    <span>Chittagong, Bangladesh</span>
+            <div className="flex  justify-between mt-4">
+              <div className="flex justify-start items-start space-x-4 ">
+                <img src={company2} alt="" className="w-32" />
+                <div className="flex flex-col bg-red h-full">
+                  <div className="tex">
+                    <h3 className="font-bold text-3xl mb-2">UI/UX Designer</h3>
+                    <div className="flex items-center space-x-3 ">
+                      <img src={location} alt="" className="w-[8%] " />
+                      <span>Chittagong, Bangladesh</span>
+                    </div>
+                    <div className="flex items-center space-x-3 ">
+                      <img src={bag} alt="" className="w-[8%] " />
+                      <span>On-Site Job</span>
+                    </div>
+                    <div className="flex items-center space-x-3 ">
+                      <img src={time} alt="" className="w-[8%] " />
+                      <span>5 Days Remaining</span>
+                    </div>
+                    {/* <p>On-Site Job</p>
+                  <p className="">5 Days Remaining</p> */}
                   </div>
-                  <p>On-Site Job</p>
-                  <p className="">5 Days Remaining</p>
                 </div>
               </div>
+              <button className="bg-[#00D4D6] w-fit h-10 px-4 rounded-xl text-white">
+                Apply Now
+              </button>
+            </div>
+            <div className="mt-5 ">
+              <h3 className="text-2xl font-bold">About The Job</h3>
+              <p>
+                Just gonna keep things like these , will reorganize segments
+                later. Scrambling faster and over and over again may get me
+                something . The motto is to keep reorganizing and getting the
+                best layout. Better to keep only logo/photos of hiring
+                companies, putting images or symbols of every occupation could
+                be painful and...
+              </p>
             </div>
           </div>
         </div>
