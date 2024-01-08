46de8ae054812b724b8a0000c1e0751ba13a3fc8
Rohan1279 <muniruddinrohan@gmail.com>
2024-01-09 00:23:42 +0600

    Add Sponser component to Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Advance from "./Advance/Advance";
 import Banner from "./Banner/Banner";
 import Careers from "./Careers/Careers";
 import Courses from "./Courses/Courses";
+import Sponser from "./Sponser/Sponser";
 import Team from "./Team/Team";
 import Testimonials from "./Testimonials/Testimonials";
 import ThemeSection from "./ThemeSection/ThemeSection";
@@ -20,6 +21,7 @@ const Home = () => {
       <UniqueSection />
       <Advance />
       <Courses />
+      <Sponser />
       <About />
       <Team />
     </>
---
+++ src/Pages/Home/Sponser/Sponser.jsx
@@ -0,0 +1,47 @@
+import supporter from "../../../assets/supporter.png";
+import sponser1 from "../../../assets/Sponsers/sponser 1.png";
+import sponser2 from "../../../assets/Sponsers/sponser 2.png";
+import sponser3 from "../../../assets/Sponsers/sponser 3.png";
+import sponser4 from "../../../assets/Sponsers/sponser 4.png";
+import sponser5 from "../../../assets/Sponsers/sponser 5.png";
+
+const Sponser = () => {
+  return (
+    <section className="text-[#595959] py-10">
+      <div className="max-w-7xl mx-auto relative">
+        <div className="flex justify-between mb-7">
+          <div className="flex space-x-2">
+            <img src={supporter} alt="supporter-icon" className="w-10 h-10" />
+            <div>
+              <h2 className="text-2xl font-bold">Our Supporters</h2>
+              <p className="text-base">
+                Look for the jobs that suit you and start applying now
+              </p>
+              <div className="grid grid-cols-5 justify-between mt-16">
+                <div className=" border-r border-[#595959] px-5">
+                  {" "}
+                  <img src={sponser1} alt="" className=" " />
+                </div>
+                <div className=" border-r border-[#595959] px-5">
+                  {" "}
+                  <img src={sponser2} alt="" className="" />
+                </div>
+                <div className=" border-r border-[#595959] px-5">
+                  <img src={sponser4} alt="" className="" />
+                </div>
+                <div className="border-r border-[#595959] px-5">
+                  <img src={sponser3} alt="" className="" />
+                </div>
+                <div className="border-r border-[#595959] px-5">
+                  <img src={sponser5} alt="" className="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Sponser;
