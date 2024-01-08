3d82a16055e08b4a6e23f53ae60e167650cdb745
Rohan1279 <muniruddinrohan@gmail.com>
2024-01-09 01:56:46 +0600

    Refactor UniqueSection component and add background image and icons

--- src/Pages/Home/UniqueSection/UniqueSection.jsx
+++ src/Pages/Home/UniqueSection/UniqueSection.jsx
@@ -1,21 +1,45 @@
+import background from "../../../assets/Doodle.png";
+import connection from "../../../assets/UniqueSectionIcons/connection.png";
+import empower from "../../../assets/UniqueSectionIcons/empower.png";
+import solution from "../../../assets/UniqueSectionIcons/solution.png";
+import tech from "../../../assets/UniqueSectionIcons/tech.png";
 const UniqueSection = () => {
   return (
-    <section className=" bg-[#00D4D6] text-white py-24">
-      <div className=" max-w-7xl mx-auto ">
-        <h2 className="font-bold text-3xl">What Makes Us Unique</h2>
-        <p className="text-base mt-3 ">
-          We are modernising the way students become elligible for top academic
-          institutions and organizations of the world regardless of the
-          location.
-        </p>
-        <ul className="text-xl font-bold list-item  list-disc ml-5 mt-3">
-          <li>Direct Connections With Top Universities & Organizations</li>
-          <li>One Stop Solution For Your Higher Education.</li>
-          <li>Empowering Youth With Job-Ready Skills Globally.</li>
-          <li>
-            Customed Solutions Using AI-Powered Technology & Oxford Alumni.
-          </li>
-        </ul>
+    <section
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="text-white py-24 bg-black opacity-80"
+    >
+      <div className="max-w-7xl mx-auto grid grid-cols-2">
+        <div className="col-span-1">
+          <h2 className="font-bold text-3xl">What Makes Us Unique</h2>
+          <p className="text-sm  mb-7">
+            SmartGrad stands out by offering access to global universities,
+            direct organizational connections, skill development led by Oxford
+            alumni, and comprehensive career guidance.
+          </p>
+          <div className="flex flex-col items-start">
+            <div className="flex justify-start items-center space-x-3 hover:bg-[#00D4D6]/50 cursor-pointer [&_img]:hover:scale-105 transition-all px-5 py-3 bg-[#D9D9D9]/50  w-96 rounded-xl my-2">
+              <img src={connection} alt="" />
+              <p>Global access to top universities</p>
+            </div>
+            <div className="flex justify-start items-center space-x-3 hover:bg-[#00D4D6]/50 cursor-pointer [&_img]:hover:scale-105 transition-all px-5 py-3 bg-[#D9D9D9]/50  w-96 rounded-xl my-2">
+              <img src={solution} alt="" />
+              <p>Direct links to leading organizations</p>
+            </div>
+            <div className="flex justify-start items-center space-x-3 hover:bg-[#00D4D6]/50 cursor-pointer [&_img]:hover:scale-105 transition-all px-5 py-3 bg-[#D9D9D9]/50  w-96 rounded-xl my-2">
+              <img src={empower} alt="" />
+              <p>Empowering with job-ready skills</p>
+            </div>
+            <div className="flex justify-start items-center space-x-3 hover:bg-[#00D4D6]/50 cursor-pointer [&_img]:hover:scale-105 transition-all px-5 py-3 bg-[#D9D9D9]/50  w-96 rounded-xl my-2">
+              <img src={tech} alt="" />
+              <p>Connect globally, grow professionally</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
