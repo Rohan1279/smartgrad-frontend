458f6287a346813f706326948fa1a6985cef50e8
Rohan1279 <muniruddinrohan@gmail.com>
2024-01-15 13:26:53 +0600

    Update testimonials section with new icons and content

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -10,9 +10,13 @@ import TestomonialCard from "./TestomonialCard";
 import student1 from "../../../assets/Students/student 1.png";
 import student2 from "../../../assets/Students/student 2.png";
 import student3 from "../../../assets/Students/student 3.png";
+import icon1 from "../../../assets/TestimonialIcons/Career.png";
+import icon2 from "../../../assets/TestimonialIcons/fa-solid_user-graduate.png";
+import icon3 from "../../../assets/TestimonialIcons/Group 33.png";
+
 const Testimonials = () => {
   return (
-    <section className=" text-[#595959] pt-[86px] pb-64 bg-[#F9F9F9] relative mb-[100px]">
+    <section className=" text-[#595959] py-[86px] bg-[#F9F9F9] relative mb-[100px]">
       <div className="max-w-7xl mx-auto ">
         {/* SECTION HEADING */}
         <div className="flex justify-between mb-7">
@@ -31,7 +35,7 @@ const Testimonials = () => {
           </div>
         </div>
 
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center mb-36">
           <div className="bg-[#595959] text-white rounded-xl   px-10 py-8 text-center  w-[400px] h-[185px] ">
             <p className="relative text-xs  ">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -72,18 +76,52 @@ const Testimonials = () => {
             </p>
           </div>
         </div>
-        <div className="text-right absolute bottom-14 translate-x-full">
-          <p>
-            <span className="font-bold"> 478+</span> Individuals have shown
-            interest and also applied to the universities via{" "}
-            <span className="font-bold lowercase">
-              smart<span className="text-[#09D5D7]">grad</span>
-            </span>
-            .
-          </p>
-          <p>
-            <span className="font-bold"> 100+</span> Applications were
-            confirmed. Congratulations to the applicants.{" "}
+        <div className="text-right mt-20">
+          <h2 className="text-2xl font-bold text-center">
+            Helping Small Business to Grow and Expand
+          </h2>
+          <p className="text-center text-[20px]">More Than</p>
+          <div className="grid grid-cols-4 mt-[60px]">
+            <div>
+              <div className="w-20 h-20 mx-auto">
+                <img src={icon1} alt="" className=" " />
+              </div>
+              <div className=" my-2">
+                <h3 className="text-center text-3xl font-bold">33+</h3>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </div>
+            <div>
+              <div className="w-20 h-20 mx-auto">
+                <img src={icon2} alt="" className=" " />
+              </div>
+              <div className="my-2">
+                <h3 className="text-center text-3xl font-bold">33+</h3>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </div>
+            <div>
+              <div className="w-20 h-20 mx-auto">
+                <img src={icon3} alt="" className=" " />
+              </div>
+              <div className=" my-2">
+                <h3 className="text-center text-3xl font-bold">33+</h3>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </div>
+            <div>
+              <div className="w-20 h-20 mx-auto">
+                <img src={icon1} alt="" className=" " />
+              </div>
+              <div className=" my-2">
+                <h3 className="text-center text-3xl font-bold">33+</h3>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </div>
+          </div>
+          <p className="text-[20px] text-center mt-[60px]">
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+            ipsum lorem ipsum
           </p>
         </div>
       </div>
