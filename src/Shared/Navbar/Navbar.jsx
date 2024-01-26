78a90a4ba6ce2c6d239efd85d02cb501d6f25bec
Md Sakib Hossain <40665494+shfsakib@users.noreply.github.com>
2024-01-27 00:34:33 +0600

    navbar position and right button responsive done

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import search from "../../assets/search.png";
 import GlobeEnIcon from "../../assets/globe-en.png";
 import GlobeBnIcon from "../../assets/globe-bn.png";
 import { BsCaretDownFill } from "react-icons/bs";
-import { useRef, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import UniversitiesMenu from "./Menus/UniversitiesMenu";
 import CareersMenu from "./Menus/CareersMenu";
 import AptitudesMenu from "./Menus/AptitudesMenu";
@@ -36,149 +36,195 @@ export default function Navbar() {
     }
   };
   return (
-    <nav
-      onMouseLeave={() => {
-        sethovering(null);
-      }}
-      className="bg-white shadow-md text-[#595959] fixed w-full top-0 z-50"
-    >
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            {/* Logo and Brand Name */}
-            <div className="flex-shrink-0 flex items-center">
-              <img
-                className="block lg:hidden h-8 w-auto"
-                src={NavIcon}
-                alt="Your Logo"
-              />
-              <img
-                className="hidden lg:block h-8 w-auto"
-                src={NavIcon}
-                alt="Your Logo"
-              />
-            </div>
-            {/* Navigation Links */}
-            <div className="hidden sm:flex justify-center items-center space-x-11 sm:ml-8 ">
-              <a
-                onMouseEnter={(e) => {
-                  onMouseEnter(1, e.currentTarget);
-                }}
-                href=""
-                className="flex items-center justify-center space-x-2"
+    <Fragment>
+      <nav
+        onMouseLeave={() => {
+          sethovering(null);
+        }}
+        class="bg-white shadow-md text-[#595959] sticky w-full top-0 z-50"
+      >
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div class="relative flex h-16 items-center justify-between">
+            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <button
+                type="button"
+                class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
               >
-                <img src={UniversitiesIcon} alt="" className="w-4" />
-                <span className="">{t("navbar.universities")}</span>
-              </a>
-              <a
-                onMouseEnter={(e) => {
-                  onMouseEnter(2, e.currentTarget);
-                }}
-                href=""
-                className="flex items-center justify-center space-x-2 "
-              >
-                <img src={CareerIcon} alt="" className="w-4" />
-                <span className="">{t("navbar.careers")}</span>
-              </a>
-              <a
-                onMouseEnter={(e) => {
-                  onMouseEnter(3, e.currentTarget);
-                }}
-                href=""
-                className="flex items-center justify-center space-x-2 "
-              >
-                <img src={AptitudeIcon} alt="" className="w-4" />
-                <span className="">{t("navbar.aptitudes")}</span>
-              </a>
-              <a
-                onMouseEnter={(e) => {
-                  onMouseEnter(4, e.currentTarget);
-                }}
-                href=""
-                className="flex items-center justify-center space-x-2 "
-              >
-                <img src={NetworkIcon} alt="" className="w-4" />
-                <span className="">{t("navbar.networks")}</span>
-              </a>
-            </div>
-          </div>
+                <span class="absolute -inset-0.5"></span>
+                <span class="sr-only">Open main menu</span>
 
-          <div
-            className={clsx(
-              "absolute top-12  -ml-2  duration-500",
-              hovering
-                ? "transition-all opacity-100 "
-                : "opacity-0 pointer-events-none"
-            )}
-            style={{ left: popOverLeft || 0 }}
-          >
+                <svg
+                  class="block h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+
+                <svg
+                  class="hidden h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div class="flex flex-shrink-0 items-center">
+                <img
+                  className="block lg:hidden h-8 w-auto"
+                  src={NavIcon}
+                  alt="Your Logo"
+                />
+                <img
+                  className="hidden lg:block h-8 w-auto"
+                  src={NavIcon}
+                  alt="Your Logo"
+                />
+              </div>
+              <div class="hidden sm:ml-6 sm:block">
+                <div class="flex space-x-4">
+                  <a
+                    onMouseEnter={(e) => {
+                      onMouseEnter(1, e.currentTarget);
+                    }}
+                    href=""
+                    className="flex items-center justify-center space-x-2 px-3 py-2 text-sm font-medium"
+                  >
+                    <img src={UniversitiesIcon} alt="" className="w-4" />
+                    <span className="">{t("navbar.universities")}</span>
+                  </a>
+                  <a
+                    onMouseEnter={(e) => {
+                      onMouseEnter(2, e.currentTarget);
+                    }}
+                    href=""
+                    className="flex items-center justify-center space-x-2 "
+                  >
+                    <img src={CareerIcon} alt="" className="w-4" />
+                    <span className="">{t("navbar.careers")}</span>
+                  </a>
+                  <a
+                    onMouseEnter={(e) => {
+                      onMouseEnter(3, e.currentTarget);
+                    }}
+                    href=""
+                    className="flex items-center justify-center space-x-2 "
+                  >
+                    <img src={AptitudeIcon} alt="" className="w-4" />
+                    <span className="">{t("navbar.aptitudes")}</span>
+                  </a>
+                  <a
+                    onMouseEnter={(e) => {
+                      onMouseEnter(4, e.currentTarget);
+                    }}
+                    href=""
+                    className="flex items-center justify-center space-x-2 "
+                  >
+                    <img src={NetworkIcon} alt="" className="w-4" />
+                    <span className="">{t("navbar.networks")}</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* submenu's */}
             <div
-              style={{
-                height: popOverHeight || 100,
-                width: popOverWidth,
-              }}
-              className="bg-white overflow-hidden  transform-gpu rounded-xl shadow-lg transition-all duration-500 "
+              className={clsx(
+                "absolute top-12  -ml-2  duration-500",
+                hovering
+                  ? "transition-all opacity-100 "
+                  : "opacity-0 pointer-events-none"
+              )}
+              style={{ left: popOverLeft || 0 }}
             >
-              <SlideWrapper index={1} hovering={hovering}>
-                <UniversitiesMenu
-                  setPopOverWidth={setPopOverWidth}
-                  ref={(element) => (refs.current[1] = element)}
-                />
-              </SlideWrapper>
-              <SlideWrapper index={2} hovering={hovering}>
-                <CareersMenu
-                  setPopOverWidth={setPopOverWidth}
-                  ref={(element) => (refs.current[2] = element)}
-                />
-              </SlideWrapper>
-              <SlideWrapper index={3} hovering={hovering}>
-                <AptitudesMenu
-                  setPopOverWidth={setPopOverWidth}
-                  ref={(element) => (refs.current[3] = element)}
-                />
-              </SlideWrapper>
-              <SlideWrapper index={4} hovering={hovering}>
-                <NetworksMenu
-                  setPopOverWidth={setPopOverWidth}
-                  ref={(element) => (refs.current[4] = element)}
-                />
-              </SlideWrapper>
+              <div
+                style={{
+                  height: popOverHeight || 100,
+                  width: popOverWidth,
+                }}
+                className="bg-white overflow-hidden  transform-gpu rounded-xl shadow-lg transition-all duration-500 "
+              >
+                <SlideWrapper index={1} hovering={hovering}>
+                  <UniversitiesMenu
+                    setPopOverWidth={setPopOverWidth}
+                    ref={(element) => (refs.current[1] = element)}
+                  />
+                </SlideWrapper>
+                <SlideWrapper index={2} hovering={hovering}>
+                  <CareersMenu
+                    setPopOverWidth={setPopOverWidth}
+                    ref={(element) => (refs.current[2] = element)}
+                  />
+                </SlideWrapper>
+                <SlideWrapper index={3} hovering={hovering}>
+                  <AptitudesMenu
+                    setPopOverWidth={setPopOverWidth}
+                    ref={(element) => (refs.current[3] = element)}
+                  />
+                </SlideWrapper>
+                <SlideWrapper index={4} hovering={hovering}>
+                  <NetworksMenu
+                    setPopOverWidth={setPopOverWidth}
+                    ref={(element) => (refs.current[4] = element)}
+                  />
+                </SlideWrapper>
+              </div>
             </div>
-          </div>
-
-          {/* Profile and Settings */}
-          <div className="absolute inset-y-0 right-0 flex space-x-7 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {i18next.language === "en" ? (
+            {/* profile & settings */}
+            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              {i18next.language === "en" ? (
+                <img
+                  onClick={() => {
+                    i18next.changeLanguage("bn");
+                  }}
+                  src={GlobeBnIcon}
+                  alt="search-icon"
+                  className="w-7 cursor-pointer hover:scale-110 transition-all active:scale-100"
+                />
+              ) : (
+                <img
+                  onClick={() => {
+                    i18next.changeLanguage("en");
+                  }}
+                  src={GlobeEnIcon}
+                  alt="search-icon"
+                  className="w-7 cursor-pointer hover:scale-110 transition-all active:scale-100"
+                />
+              )}
               <img
-                onClick={() => {
-                  i18next.changeLanguage("bn");
-                }}
-                src={GlobeBnIcon}
+                src={search}
                 alt="search-icon"
-                className="w-7 cursor-pointer hover:scale-110 transition-all active:scale-100"
+                className="w-5 ml-3 cursor-pointer"
               />
-            ) : (
-              <img
-                onClick={() => {
-                  i18next.changeLanguage("en");
-                }}
-                src={GlobeEnIcon}
-                alt="search-icon"
-                className="w-7 cursor-pointer hover:scale-110 transition-all active:scale-100"
-              />
-            )}
 
-            <img
-              src={search}
-              alt="search-icon"
-              className="w-5 cursor-pointer"
-            />
-            <button className="bg-[#09D5D7] hover:shadow-md transition-all text-white text-[12px] rounded-xl px-4 py-2">
-              Log In
-            </button>
+              <div class="relative ml-3">
+                <div>
+                  <button className="bg-[#09D5D7] hover:shadow-md transition-all text-white text-[12px] rounded-xl px-4 py-2">
+                    Log In
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </Fragment>
   );
 }
