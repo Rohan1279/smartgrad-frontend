b1c57f783df1ce0007441dc2e8aadb7ddac1500c
Md Sakib Hossain <40665494+shfsakib@users.noreply.github.com>
2024-01-28 16:21:11 +0600

    testimonial carousel done

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,46 +1,110 @@
-import testimonial from "../../../assets/testimonial.png";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "../../../components/ui/carousel-aptitude";
-
-import student1 from "../../../assets/Students/student 4.png";
-import student2 from "../../../assets/Students/student 5.png";
-import student3 from "../../../assets/Students/student 6.png";
 import icon1 from "../../../assets/TestimonialIcons/Career.png";
 import icon2 from "../../../assets/TestimonialIcons/fa-solid_user-graduate.png";
 import icon3 from "../../../assets/TestimonialIcons/Group 33.png";
 import AnimatedNumber from "../../../components/AnimatedNumber";
 import useWindowDimensions from "../../../components/core/windowsDimention";
 import TestimonialList from "../../../assets/dummy/testimonial.json";
-import { Swiper, SwiperSlide } from "swiper/react";
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import { Autoplay, Pagination, Navigation } from "swiper";
 import { useEffect, useRef, useState } from "react";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
+// Import Swiper styles
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Testimonials = () => {
   const { width } = useWindowDimensions();
-  const [activeInx, setActiveInx] = useState(0);
-  const owlCarouselRef = useRef(null);
-  const handleIndexChanged = (e) => {
-    const currentIndex = e.item.index;
-    console.log(currentIndex);
-    // setActiveInx(currentIndex);
+  const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
+  const [currentInx, setCurrentInx] = useState(width > 1215 ? 1 : 0);
+  const [prevInx, setPrevInx] = useState(0);
+  const [activeInx, setActiveInx] = useState(width > 1215 ? 1 : 0);
+  const slider = useRef();
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: width > 1215 ? 3 : width > 868 ? 2 : 1,
+    slidesToScroll: 1,
+    beforeChange: (current, inx) => {
+      console.log(width);
+      if (width > 1215) {
+        if (TestimonialList.length - 2 === inx) {
+          setActiveInx(inx + 1);
+          setCurrentInx(inx + 1);
+          setPrevInx(inx);
+        } else if (TestimonialList.length - 1 === inx) {
+          setActiveInx(0);
+          setCurrentInx(0);
+          setPrevInx(-1);
+        } else if (inx > -1) {
+          setActiveInx(inx + 1);
+          setCurrentInx(inx + 1);
+          setPrevInx(inx);
+        } else {
+          setActiveInx(inx);
+          setCurrentInx(inx);
+          setPrevInx(inx - 1);
+        }
+      } else {
+        if (TestimonialList.length - 2 === inx) {
+          setActiveInx(inx);
+          setCurrentInx(inx);
+          setPrevInx(inx - 1);
+        } else if (TestimonialList.length - 1 === inx) {
+          setActiveInx(TestimonialList.length - 1);
+          setCurrentInx(TestimonialList.length - 1);
+          setPrevInx(TestimonialList.length - 2);
+        } else if (inx > -1) {
+          setActiveInx(inx);
+          setCurrentInx(inx);
+          setPrevInx(inx - 1);
+        } else {
+          setActiveInx(inx);
+          setCurrentInx(inx);
+          setPrevInx(inx - 1);
+        }
+      }
+    },
+    // afterChange: (inx) => {
+    //   console.log("aft" + inx);
+    //   console.log("aftcure" + currentInx);
+    //   if (inx !== 0 && currentInx !== 0 && inx < currentInx) {
+    //     setActiveInx(inx - 1);
+    //     setCurrentInx(inx - 1);
+    //   }
+    // },
   };
-  const options = {
-    items: width > 992 ? 3 : width > 768 ? 2 : 1,
-    loop: true,
-    margin: 10,
-    autoplay: false,
-  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newWindowWidth = window.innerWidth;
+
+      // Compare current and previous window width
+      if (newWindowWidth > currentWidth) {
+        // console.log("Window is increasing in size (from small to large).");
+        if (width > 1215) {
+          setActiveInx(currentInx + 1);
+        } else if (width < 1215) {
+          setActiveInx(currentInx);
+        }
+      } else if (newWindowWidth < currentWidth) {
+        // console.log("Window is decreasing in size (from large to small).");
+        if (width < 1215) {
+          setActiveInx(prevInx);
+        }
+      }
+
+      // Update the state with the new window width
+      setCurrentWidth(newWindowWidth);
+    };
+
+    // Add event listener for resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [currentWidth]);
 
   return (
     <section className=" text-[#595959] py-[86px] bg-[#F9F9F9] relative mb-[100px] px-4">
@@ -62,27 +126,24 @@ const Testimonials = () => {
           </div>
         </div>
 
-        <div className="flex justify-between items-center mb-36 px-4">
-          <OwlCarousel
-            ref={owlCarouselRef}
-            className="owl-theme testimonialSwiper"
-            onChange={(e) => {
-              handleIndexChanged(e);
-            }}
-            {...options}
+        <div className="flex flex-col justify-between items-center mb-36 px-4">
+          <Slider
+            ref={slider}
+            className="custom-carousel-container"
+            {...settings}
           >
             {TestimonialList.map((item, index) => (
-              <div className="item" key={index}>
+              <div className="custom-carousel-item" key={index}>
                 <div
                   className={`${
-                    index !== -1
+                    index === activeInx
                       ? "bg-[#D9D9D9] text-[#595959] pt-[40px] w-[450px] h-[214px] shadow-lg"
                       : "bg-[#595959] text-white py-8 w-[400px] h-[185px] shadow-md leading-3"
-                  } rounded-xl px-10 text-center relative testimonial-card`}
+                  } rounded-xl px-4 sm:px-10 text-center relative testimonial-card ease-in-out duration-300`}
                 >
                   <p
                     className={`${
-                      index !== -1
+                      index === activeInx
                         ? "text-left mt-11 text-5xl h-4"
                         : "text-left mt-8 text-[33px] h-1"
                     }`}
@@ -92,7 +153,7 @@ const Testimonials = () => {
                   <p className="text-xs text-right">{item?.content}</p>
                   <p
                     className={`${
-                      index !== -1
+                      index === activeInx
                         ? "text-5xl text-right "
                         : "text-[33px] text-right mt-3"
                     }`}
@@ -101,12 +162,12 @@ const Testimonials = () => {
                   </p>
                   <div
                     className={`bg-transparent left-6 flex absolute ${
-                      index !== -1 ? "-top-14" : "text-white -top-8"
+                      index === activeInx ? "-top-14" : "text-white -top-8"
                     } items-end text-left space-x-[14px]`}
                   >
                     <div
                       className={`${
-                        index !== -1
+                        index === activeInx
                           ? "w-[130px] h-[130px]"
                           : "w-[74px] h-[74px]"
                       } rounded-lg overflow-hidden`}
@@ -118,7 +179,9 @@ const Testimonials = () => {
                       />
                     </div>
                     <div
-                      className={`leading-3 ${index !== -1 ? "mb-5" : "mb-2 "}`}
+                      className={`leading-3 ${
+                        index === activeInx ? "mb-5" : "mb-2 "
+                      }`}
                     >
                       <p className="text-sm">{item?.name}</p>
                       <p className="text-md font-bold">{item?.role}</p>
@@ -127,77 +190,26 @@ const Testimonials = () => {
                 </div>
               </div>
             ))}
-            {/* Add more items as needed */}
-          </OwlCarousel>
-          {/* <Carousel className="testimonialSwiper" set>
-            <CarouselContent className="py-5 item-Container">
-              {TestimonialList.map((item, index) => (
-                <CarouselItem
-                  className="sliderGroup basis-full lg:basis-1/3"
-                  key={index}
-                >
-                  <div
-                    className={`${
-                      index !== -1
-                        ? "bg-[#D9D9D9] text-[#595959] pt-[40px] w-[450px] h-[214px] shadow-lg"
-                        : "bg-[#595959] text-white py-8 w-[400px] h-[185px] shadow-md leading-3"
-                    } rounded-xl px-10 text-center relative`}
-                  >
-                    <p
-                      className={`${
-                        index !== -1
-                          ? "text-left mt-11 text-5xl h-4"
-                          : "text-left mt-8 text-[33px] h-1"
-                      }`}
-                    >
-                      “
-                    </p>
-                    <p className="text-xs text-right">{item?.content}</p>
-                    <p
-                      className={`${
-                        index !== -1
-                          ? "text-5xl text-right "
-                          : "text-[33px] text-right mt-3"
-                      }`}
-                    >
-                      ”
-                    </p>
-                    <div
-                      className={`bg-transparent left-6 flex absolute ${
-                        index !== -1 ? "-top-14" : "text-white -top-8"
-                      } items-end text-left space-x-[14px]`}
-                    >
-                      <div
-                        className={`${
-                          index !== -1
-                            ? "w-[130px] h-[130px]"
-                            : "w-[74px] h-[74px]"
-                        } rounded-lg overflow-hidden`}
-                      >
-                        <img
-                          src={item?.image}
-                          alt=""
-                          className=" object-fill mx-auto"
-                        />
-                      </div>
-                      <div
-                        className={`leading-3 ${
-                          index !== -1 ? "mb-5" : "mb-2 "
-                        }`}
-                      >
-                        <p className="text-sm">{item?.name}</p>
-                        <p className="text-md font-bold">{item?.role}</p>
-                      </div>
-                    </div>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <div className="flex justify-center">
-              <CarouselPrevious className="" />
-              <CarouselNext className="" />
-            </div>
-          </Carousel> */}
+          </Slider>
+          <div className="flex mt-8 gap-4">
+            <button
+              className="slick-prev-btn"
+              onClick={() => {
+                slider.current.slickPrev();
+              }}
+            >
+              &lt;
+            </button>
+            <button
+              className="slick-next-btn"
+              onClick={() => {
+                slider.current.slickNext();
+              }}
+            >
+              {" "}
+              &gt;
+            </button>
+          </div>
         </div>
         <div className="text-right mt-20">
           <h2 className="text-2xl font-bold text-center">
