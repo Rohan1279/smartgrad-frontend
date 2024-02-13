40b9ea9ecbe0da510ba8dadd80d12c5e2b872537
JOY-JB <joybarua0058@gmail.com>
2024-02-14 01:07:03 +0600

    third section redesign done

--- src/Pages/Home/UniqueSection/UniqueSection.jsx
+++ src/Pages/Home/UniqueSection/UniqueSection.jsx
@@ -1,21 +1,15 @@
-import background from "../../../assets/unique-section.png";
-import placeholder from "../../../assets/unique-section-placeholder.png";
-import connection from "../../../assets/UniqueSectionIcons/connection.png";
-import empower from "../../../assets/UniqueSectionIcons/empower.png";
-import solution from "../../../assets/UniqueSectionIcons/solution.png";
-import tech from "../../../assets/UniqueSectionIcons/tech.png";
+import background from "../../../assets/Hero-Banner-Bg.png";
 
 // import UniversitiesIcon from "../../../assets/convocation-cap.png";
 // import CareerIcon from "../../../assets/JobIcons/bag.png";
 // import AptitudeIcon from "../../../assets/aptitude-icon.png";
 // import Aptitude from "../../../assets/ThemeImages/aptitude-icon-fill.png";
 
-import University from "../../../assets/ThemeImages/university-icon-fill.png";
-import Career from "../../../assets/ThemeImages/job-icon-fill.png";
 import Aptitude from "../../../assets/ThemeImages/aptitude-icon-fill.png";
+import Career from "../../../assets/ThemeImages/job-icon-fill.png";
 import Network from "../../../assets/ThemeImages/network-icon-fill.png";
+import University from "../../../assets/ThemeImages/university-icon-fill.png";
 
-import NetworkIcon from "../../../assets/network-icon.svg";
 
 import "./UniqueSection.css";
 const UniqueSection = () => {
@@ -26,13 +20,13 @@ const UniqueSection = () => {
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
       }}
-      className="text-white py-24 mb-[100px] px-4"
+      className="text-primary py-32 mb-[100px] px-4"
     >
       <div className="max-w-7xl mx-auto relative">
         <div className="flex justify-between mb-7">
           <div className="flex space-x-2">
             <div>
-              <h2 className="text-3xl font-bold">Why is smartgrad for you</h2>
+              <h2 className="text-4xl font-bold">Why is smartgrad for you</h2>
               <p className="text-base max-w-xl mt-4">
                 SmartGrad stands out by offering access to global universities,
                 direct organizational connections, skill development led by
@@ -44,28 +38,28 @@ const UniqueSection = () => {
         <div className="grid grid-cols-1 lsm:grid-cols-2 gap-x-8">
           <div className="order-2 mmd:order-1 col-span-1">
             <div className="flex flex-col items-start features-column">
-              <div className="flex justify-start items-center space-x-3 hover:bg-primary/50 cursor-pointer  transition-all duration-500 px-5 py-3 bg-[#D9D9D9]/20 w-full lsm:w-[25rem] h-[70px] rounded-xl my-2 ">
+              <div className="flex justify-start items-center space-x-3 hover:bg-primary/50 cursor-pointer  transition-all duration-500 px-5 py-3 bg-[#D9D9D9] w-full lsm:w-[25rem] h-[70px] rounded-xl my-2">
                 <img src={University} alt="" className="w-[25px]" />
                 <p>
                   Use AI and Oxford expertise for a smooth university
                   application process.
                 </p>
               </div>
-              <div className="flex justify-start items-center space-x-3 hover:bg-primary/50 cursor-pointer  transition-all duration-500 px-5 py-3 bg-[#D9D9D9]/20  w-full lsm:w-[25rem] h-[70px] rounded-xl my-2 ">
+              <div className="flex justify-start items-center space-x-3 hover:bg-primary/50 cursor-pointer  transition-all duration-500 px-5 py-3 bg-[#D9D9D9]  w-full lsm:w-[25rem] h-[70px] rounded-xl my-2 ">
                 <img src={Career} alt="" className="w-[25px]" />
                 <p>
                   Direct links to leading organizations & Never manually update
                   your cv again
                 </p>
               </div>
-              <div className="flex justify-start items-center space-x-3 hover:bg-primary/50 cursor-pointer  transition-all duration-500 px-5 py-3 bg-[#D9D9D9]/20  w-full lsm:w-[25rem] h-[70px] rounded-xl my-2 ">
+              <div className="flex justify-start items-center space-x-3 hover:bg-primary/50 cursor-pointer  transition-all duration-500 px-5 py-3 bg-[#D9D9D9]  w-full lsm:w-[25rem] h-[70px] rounded-xl my-2 ">
                 <img src={Aptitude} alt="" className="w-[25px]" />
                 <p>
                   Boost your abilities with customized courses aimed at
                   achieving success.
                 </p>
               </div>
-              <div className="flex justify-start items-center space-x-3 hover:bg-primary/50 cursor-pointer  transition-all duration-500 px-5 py-3 bg-[#D9D9D9]/20  w-full lsm:w-[25rem] h-[70px] rounded-xl my-2 ">
+              <div className="flex justify-start items-center space-x-3 hover:bg-primary/50 cursor-pointer  transition-all duration-500 px-5 py-3 bg-[#D9D9D9]  w-full lsm:w-[25rem] h-[70px] rounded-xl my-2 ">
                 <img src={Network} alt="" className="w-[25px]" />
                 <p>
                   Get sector expert advice from global industry professionals
@@ -74,10 +68,8 @@ const UniqueSection = () => {
               </div>
             </div>
           </div>
-          <div className="order-1 mmd:order-2 flex items-center justify-center lsm:justify-end pt-0 mmd:pt-8 pb-8 mmd:pb-0">
-            <div>
-              <img src={placeholder} alt="" className="w-[408px] h-[408px]" />
-            </div>
+          <div className="order-1 mmd:order-2 flex items-center justify-center relative -top-20 -right-20">
+            <iframe width="560" height="315" className="rounded-xl shadow-2xl" src="https://www.youtube.com/embed/_TPzFym3rVU?si=kumwgu10teIduJml" title="Smartgrad" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen ></iframe>
           </div>
         </div>
       </div>
